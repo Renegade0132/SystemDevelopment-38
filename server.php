356cb49b801d7e214bb65861a53939c946d2c37c
jdawithers <79966668+jdawithers@users.noreply.github.com>
2024-05-07 12:00:18 +0200

    Added server files
    Added a new file to the cluster of PHP files, in order to develop and create the functional server.

---
+++ server.php
@@ -0,0 +1,49 @@
+<?php
+require 'vendor/autoload.php';
+require_once 'Database.php';
+require_once 'User.php';
+require_once 'Course.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class MyChat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        // Store the new connection
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        // Handle incoming messages
+        // You'll need to parse the message to determine what the client is asking for,
+        // then use your User, Course, and Database classes to fulfill the request
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        // Remove the connection when it closes
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        // Handle errors
+        $conn->close();
+    }
+}
+
+// Run the server
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new MyChat()
+        )
+    ),
+    8080
+);
+
+$server->run();
